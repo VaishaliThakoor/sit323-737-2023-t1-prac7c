259e77e362a7e65ee6ec2d9005cdcdf92cdf165d
mthakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-04-15 00:43:36 +1000

    commit changes

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
