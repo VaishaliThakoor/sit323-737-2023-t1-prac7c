483640e6466c2e98610f54222b8d69e3da1104d4
mthakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-05-05 23:51:14 +1000

    changes

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const express = require('express')
-const app = express()
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
 
-app.get('/health', (req, res) => {
-  res.send('OK')
-})
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+    req.on('end', () => {
+      const params = querystring.parse(body);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<h1>Form Data Received:</h1>');
+      res.write(`<p>Name: ${params.name}</p>`);
+      res.write(`<p>Email: ${params.email}</p>`);
+      res.end();
+    });
+  } else {
+    const parsedUrl = url.parse(req.url, true);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<form method="POST">');
+    res.write('<label for="name">Name:</label>');
+    res.write('<input type="text" name="name" id="name" required>');
+    res.write('<br>');
+    res.write('<label for="email">Email:</label>');
+    res.write('<input type="email" name="email" id="email" required>');
+    res.write('<br>');
+    res.write('<input type="submit" value="Submit">');
+    res.write('</form>');
+    res.end();
+  }
+});
+
+server.listen(7000, () => {
+  console.log('Server running on port 7000');
+});
